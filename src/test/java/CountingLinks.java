a7a2b3f34051d8b0b123d8dfd11c05d105d376ea
KARTIK AHALAWAT <kartikahalawat01@gmail.com>
2026-09-25 13:31:43 +0530

    Counting Links on whole website

--- src/test/java/CountingLinks.java
+++ src/test/java/CountingLinks.java
@@ -2,10 +2,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CountingLinks {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://qaclickacademy.com/practice.php");
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
+        int linkCount = driver.findElements(org.openqa.selenium.By.tagName("a")).size();
+        System.out.println("Number of links: " + linkCount);
 
     }
 }
