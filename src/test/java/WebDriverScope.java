6eb9b3ae3f32787aec069d5ede1e51f692d2dadc
KARTIK AHALAWAT <kartikahalawat01@gmail.com>
2026-09-25 13:56:34 +0530

    Opening Seperate links simultaneously on new tabs

---
+++ src/test/java/WebDriverScope.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverScope {
+    static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        WebElement footerDriver = driver.findElement(By.id("gf-BIG"));
+        int footerLinkCount = footerDriver.findElements(By.tagName("a")).size();
+        System.out.println("Number of links in footer: " + footerLinkCount);
+
+        // Limiting the scope to first column of footer
+        WebElement columnDriver = footerDriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
+        int columnLinkCount = columnDriver.findElements(By.tagName("a")).size();
+        System.out.println("Number of links in first column of footer: " + columnLinkCount);
+
+        //Opening each link in the first column of footer in a new tab and check if the pages are opening
+        for (int i = 1; i < columnLinkCount; i++) {
+            String clickOnLinkTab = Keys.chord(Keys.CONTROL, Keys.ENTER);
+            columnDriver.findElements(By.tagName("a")).get(i).sendKeys(clickOnLinkTab);
+        }
+
+    }
+}
